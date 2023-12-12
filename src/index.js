cbe911cc75b5967d9a2f6f85c1297db2d8021e9f
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2023-12-12 14:47:58 -0700

    added pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Abouts from './Pages/Abouts';
+import Admin from './Pages/Admin';
+import Carts from './Pages/Carts';
+import Error from './Pages/Error';
+import Home from './Pages/Home';
+import Products from './Pages/Products';
+
+const router = createBrowserRouter([
+  {path: '/', element: <Home />},
+  {path: '/cart', element: <Carts />},
+  {path: '/about', element: <Abouts/>},
+  {path: '/products', element: <Products />},
+  {path: '*', element: <Error />},
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
