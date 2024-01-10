4c7618df7af80eefe5088c6c193b759d59073b57
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2024-01-10 13:32:06 -0700

    update tabs project

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import {useState, useEffect} from 'react'
+import {FaAngleDoubleRight} from 'react-icons/fa'
+
+const url = 'https://course-api.com/react-tabs-project'
+function App () {
+    const [loading, setLoading] = useState(true)
+    const [jobs, setJobs] = useState([])
+    const [value, setValue] = useState(0)
+
+    const fetchJobs = async () =>{
+        const response = await fetch(url)
+        const newJobs = await response.json()
+        setJobs(newJobs)
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        fetchJobs()
+    },[])
+
+    if(loading){
+        return(
+            <section className='section loading'>
+                <h1>Loading ...</h1>
+            </section>
+        )
+    }
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import Abouts from './Pages/Abouts';
-import Admin from './Pages/Admin';
-import Carts from './Pages/Carts';
-import Error from './Pages/Error';
-import Home from './Pages/Home';
-import Products from './Pages/Products';
-
-const router = createBrowserRouter([
-  {path: '/', element: <Home/>},
-  {path: '/cart', element: <Carts/>},
-  {path: '/about', element: <Abouts/>},
-  {path: '/products', element: <Products/>},
-  {path: '*', element: <Error/>},
-])
+import App from './App';
+import './Styles/main.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App/>
   </React.StrictMode>
 );
