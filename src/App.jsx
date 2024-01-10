48e270ee49e33976af2a4f6cebdf262f7023706f
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2024-01-10 13:57:13 -0700

    finish the tabs

--- src/App.jsx
+++ src/App.jsx
@@ -21,16 +21,53 @@ function App () {
     if(loading){
         return(
             <section className='section loading'>
-                <h1>Loading ...</h1>
+                <div id="loading-bar-spinner" class="spinner">
+                    <div class="spinner-icon"></div>
+                </div>
             </section>
         )
     }
 
-  return (
-    <div>
-        
-    </div>
-  )
+    const {company, dates, duties, title} = jobs[value]
+
+    return(
+        <section className='section jobs'>
+            <div className="title">
+                <h2 style={{margin: '3rem'}}>Experience</h2>
+                <div className="underline"></div>
+            </div>
+            <div className="jobs-center">
+                <div className="btn-container">
+                    {jobs.map((item, index)=>{
+                        return(
+                            <button
+                            key={item.id}
+                            onClick={()=>setValue(index)}
+                            className={`job-btn ${index === value && 'active-btn'}`}
+                            >{item.company}</button>
+                        )
+                    })}
+                </div>
+                {/* Job Info */}
+                <article className="job-info">
+                    <h3>{title}</h3>
+                    <h4>{company}</h4>
+                    <p className="job-date">{dates}</p>
+                    {duties.map((duty, index)=>{
+                        return(
+                            <div key={index} className="job-desc">
+                                <FaAngleDoubleRight className = "job-icon"></FaAngleDoubleRight>
+                                <p>{duty}</p>
+                            </div>
+                        )
+                    })}
+                </article>
+            </div>
+            <button type='button' className='btn'>
+                More Info
+            </button>
+        </section>
+    )
 }
 
 export default App
